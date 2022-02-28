a7adfbf1a02b167b1e218d9ab11f023e770d5da2
Ben Willmore <ben@willmore.eu>
2022-02-28 19:26:43 +0000

    Light mode: Make primary color match link color for consistency

--- web-src/src/App.vue
+++ web-src/src/App.vue
@@ -180,7 +180,7 @@ export default {
       socket.onopen = function () {
         vm.$store.dispatch('add_notification', {
           text: 'Connection to server established',
-          type: 'primary',
+          type: 'success',
           topic: 'connection',
           timeout: 2000
         })
