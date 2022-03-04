af7b4280b461d9cae01ba7b5b2530ed7ebeec1d0
Ben Willmore <ben@willmore.eu>
2022-03-04 12:12:27 +0000

    Fix Safari toolbar color via meta tag

--- web-src/src/pages/SettingsPageWebinterface.vue
+++ web-src/src/pages/SettingsPageWebinterface.vue
@@ -177,13 +177,17 @@ export default {
     function setColorScheme() {
       const system_scheme = window.matchMedia("(prefers-color-scheme: dark)")
       const preferred_colorscheme = localStorage.getItem("preferred_colorscheme")
+      const meta_tag = document.querySelector( 'meta[name="theme-color"]' )
 
       if (preferred_colorscheme === "dark" ||
           (preferred_colorscheme === "system" && system_scheme.matches)) {
         document.documentElement.classList.add("dark-theme")
+        meta_tag.setAttribute('content', '#0a0a0a')
+
       } else if (preferred_colorscheme == "light" ||
           (preferred_colorscheme === "system" && !system_scheme.matches)) {
         document.documentElement.classList.remove("dark-theme")
+        meta_tag.setAttribute('content', '#f5f5f5')
       }
     };
 
